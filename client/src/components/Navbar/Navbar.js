38caf5a9e94732062f00428c8d982ceb334587a1
Jonghan Park <jonghan.park@edu.sait.ca>
2022-12-30 14:32:57 -0700

    Update nav bar

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="top">
+      <div className="topLeft">l</div>
+      <div className="topCenter">c</div>
+      <div className="topRight">r</div>
+    </div>
+  );
+};
+
+export default Navbar;

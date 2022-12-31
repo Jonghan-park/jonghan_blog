424ba93efeb63bb9efd599b2fec0903b69bf5ee8
Jonghan Park <jonghan.park@edu.sait.ca>
2022-12-31 16:04:00 -0700

    Update navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,9 +4,23 @@ import "./navbar.css";
 const Navbar = () => {
   return (
     <div className="top">
-      <div className="topLeft">l</div>
-      <div className="topCenter">c</div>
-      <div className="topRight">r</div>
+      <div className="topLeft">
+        <i className="topIcon fa-brands fa-linkedin"></i>
+        <i className="topIcon fa-brands fa-github"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img className="topImg" src="" alt="" />
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
     </div>
   );
 };

c046d9fc0d343c1fd5543ab73517ab426a04354b
Jonghan Park <jonghan.park@edu.sait.ca>
2023-01-02 09:35:48 -0700

    Update logo and navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./navbar.css";
+import logo from "../../assets/logo.png";
+import profileImg from "../../assets/bio-photo.jpg";
 
 const Navbar = () => {
   return (
     <div className="top">
       <div className="topLeft">
-        <i className="topIcon fa-brands fa-linkedin"></i>
-        <i className="topIcon fa-brands fa-github"></i>
+        <img className="topLogo" src={logo} alt="Logo" />
       </div>
       <div className="topCenter">
         <ul className="topList">
@@ -18,11 +19,9 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="topRight">
-        <img
-          className="topImg"
-          src="../assets/bio-photo.jpg"
-          alt="Profile pic"
-        />
+        <img className="topImg" src={profileImg} alt="Profile pic" />
+        <i className="topIcon fa-brands fa-linkedin"></i>
+        <i className="topIcon fa-brands fa-github"></i>
         <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
       </div>
     </div>

42bc0d8b632584971946ae34cc14d560241aa4ae
Jonghan Park <jonghan.park@edu.sait.ca>
2023-01-02 12:27:05 -0700

    Update post

--- client/src/post/Post.js
+++ client/src/post/Post.js
@@ -12,7 +12,21 @@ const post = () => {
           <span className="postCat">What I learned</span>
         </div>
         <span className="postTitle">What is React?</span>
+        <hr />
+        <span className="postDate">1 hour ago</span>
       </div>
+      <p className="postDesc">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam odio
+        molestias eveniet, saepe illum illo error culpa repudiandae magni
+        ratione quos vel iste amet doloremque ab cupiditate beatae expedita
+        rerum? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Quisquam odio molestias eveniet, saepe illum illo error culpa
+        repudiandae magni ratione quos vel iste amet doloremque ab cupiditate
+        beatae expedita rerum? Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Quisquam odio molestias eveniet, saepe illum illo
+        error culpa repudiandae magni ratione quos vel iste amet doloremque ab
+        cupiditate beatae expedita rerum?
+      </p>
     </div>
   );
 };

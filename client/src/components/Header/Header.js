8afca5e654d6432f04844fb28799c1075fe157bf
Jonghan Park <jonghan.park@edu.sait.ca>
2023-01-02 10:09:27 -0700

    Update sidebar, posts and header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./header.css";
+import headerImg from "../../assets/header-image.png";
 
 function Header() {
-  return <div className="header">header</div>;
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">Software developer</span>
+        <span className="headerTitleLg">Jonghan's Blog</span>
+      </div>
+      <img className="headerImg" src={headerImg} alt="" />
+    </div>
+  );
 }
 
 export default Header;
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import Posts from "../../posts/Posts";
+import Sidebar from "../../components/Sidebar/Sidebar";
 import "./home.css";
 
 const Home = () => {
   return (
     <div className="home">
       <Header />
-      Homepage
+      <div className="home">
+        <Posts />
+        <Sidebar />
+      </div>
     </div>
   );
 };

89170304d10293ef281192f844b89892a3cbd4f6
Jonghan Park <jonghan.park@edu.sait.ca>
2023-01-05 12:02:49 -0700

    Update react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Single from "./pages/single/Single";
@@ -7,15 +9,20 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 
 function App() {
+  const user = false;
   return (
     <div className="App">
-      <Navbar />
-      {/* <Home /> */}
-      {/* <Single /> */}
-      {/* <Write /> */}
-      {/* <Setting /> */}
-      {/* <Login /> */}
-      <Register />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/single" element={<Single />} />
+          <Route path="/write" element={user ? <Write /> : <Register />} />
+          <Route path="/setting" element={user ? <Setting /> : <Register />} />
+          <Route path="/login" element={user ? <Home /> : <Login />} />
+          <Route path="/register" element={user ? <Home /> : <Register />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./navbar.css";
 import logo from "../../assets/logo.png";
 import profileImg from "../../assets/bio-photo.jpg";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -11,16 +12,27 @@ const Navbar = () => {
       </div>
       <div className="topCenter">
         <ul className="topList">
-          <li className="topListItem">HOME</li>
-          <li className="topListItem">ABOUT</li>
-          <li className="topListItem">CONTACT</li>
-          <li className="topListItem">WRITE</li>
-          <li className="topListItem">LOGOUT</li>
+          <li className="topListItem">
+            <Link to="/">HOME</Link>
+          </li>
+          <li className="topListItem">
+            <Link to="/setting">ABOUT</Link>
+          </li>
+          <li className="topListItem">
+            <Link to="/">CONTACT</Link>
+          </li>
+          <li className="topListItem">
+            <Link to="/write">WRITE</Link>
+          </li>
+          <li className="topListItem">
+            <Link to="#">LOGOUT</Link>
+          </li>
         </ul>
       </div>
       <div className="topRight">
         <img className="topImg" src={profileImg} alt="Profile pic" />
         <i className="topIcon fa-brands fa-linkedin"></i>
+
         <i className="topIcon fa-brands fa-github"></i>
         <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
       </div>
